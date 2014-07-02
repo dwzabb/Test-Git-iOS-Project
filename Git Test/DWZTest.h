66e5d134cec3ff6642eebc74ca0f264792514e9a
邓 文哲 <DengWenzhe@dengdeMacBook-Pro.local>
2014-07-02 10:14:45 -0400

    added a test class

---
+++ Git Test/DWZTest.h
@@ -0,0 +1,13 @@
+//
+//  DWZTest.h
+//  Git Test
+//
+//  Created by 邓 文哲 on 14-7-2.
+//  Copyright (c) 2014年 邓 文哲. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DWZTest : NSObject
+
+@end
---
+++ Git Test/DWZTest.m
@@ -0,0 +1,13 @@
+//
+//  DWZTest.m
+//  Git Test
+//
+//  Created by 邓 文哲 on 14-7-2.
+//  Copyright (c) 2014年 邓 文哲. All rights reserved.
+//
+
+#import "DWZTest.h"
+
+@implementation DWZTest
+
+@end
